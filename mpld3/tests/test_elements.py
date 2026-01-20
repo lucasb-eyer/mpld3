35b39acabb6f6541d7e61c7eb51a69c78ce40beb
lucasb-eyer <lucasb.eyer.be@gmail.com>
2026-01-20 23:56:51 +0100

    More complete title support.
    This adds or fixes support for the following matplotlib axis title
    (ax.set_title) features:
    - newlines: SVG `<text>` can't do that, need to use `<tspan>`s inside,
      see also [this SO q/a](https://stackoverflow.com/questions/16701522/how-to-linebreak-an-svg-text-within-javascript).
    - linespacing: naturally follows from wanting newlines. Needs exporter
      support, so PR https://github.com/mpld3/mplexporter/pull/77 first.
    - left/right titles were completely missing; this adds support for
      those, which also needs exporter support, so this PR first:
      https://github.com/mpld3/mplexporter/pull/78
    - The `malign` property, which defines in the multi-line case, how the
      text lines should be aligned (all left, all center, all right).
    
    I made this with major help from gpt-codex-5.2, although in the end I
    went over all of its code and did a little simplifications here and
    there, needed multiple iterations overall.

--- mpld3/tests/test_elements.py
+++ mpld3/tests/test_elements.py
@@ -108,8 +108,8 @@ def test_text():
 
     assert_equal(list(sorted(text.keys())),
                  ['alpha', 'color', 'coordinates', 'fontsize', 'h_anchor',
-                  'id', 'position', 'rotation', 'text', 'v_baseline',
-                  'zorder'])
+                  'id', 'linespacing', 'm_align', 'position', 'rotation',
+                  'text', 'v_align', 'v_baseline', 'zorder'])
     assert_equal(text['alpha'], 0.7)
     assert_equal(text['color'], "#FF0000")
     assert_equal(text['text'], "abcde")
@@ -172,6 +172,18 @@ def test_figure_texts_are_exported():
     assert_equal(footer['coordinates'], "figure")
     assert_almost_equal(footer['position'][0], 0.1)
     assert_almost_equal(footer['position'][1], 0.05)
+
+
+def test_left_right_titles_exported():
+    fig, ax = plt.subplots()
+    ax.set_title("Left", loc="left")
+    ax.set_title("Right", loc="right")
+
+    rep = fig_to_dict(fig)
+    texts = [text['text'] for text in rep['axes'][0]['texts']]
+
+    assert "Left" in texts
+    assert "Right" in texts
 
 
 def test_ticks():
--- src/elements/text.js
+++ src/elements/text.js
@@ -8,6 +8,9 @@ mpld3_Text.prototype.defaultProps = {
     coordinates: "data",
     h_anchor: "start",
     v_baseline: "auto",
+    v_align: null,
+    m_align: null,
+    linespacing: 1.2,
     rotation: 0,
     fontsize: 11,
     drawstyle: "none",
@@ -42,12 +45,12 @@ mpld3_Text.prototype.draw = function() {
     this.obj
         .attr("class", "mpld3-text")
         .attr("xml:space", "preserve")
-        .text(this.text)
         .style("text-anchor", this.props.h_anchor)
         .style("dominant-baseline", this.props.v_baseline)
         .style("font-size", this.props.fontsize)
         .style("fill", this.props.color)
         .style("opacity", this.props.alpha);
+    this._setText();
     this.applyTransform();
 };
 
@@ -55,13 +58,85 @@ mpld3_Text.prototype.elements = function(d) {
     return d3.select(this.obj);
 };
 
+mpld3_Text.prototype._setText = function() {
+    var lines = this.text.split(/\r?\n/);
+    if (lines.length === 1) {
+        this.obj.text(lines[0]);
+        return;
+    }
+    this.obj.text(null);
+    var lineSpacing = this.props.linespacing;
+    var offsetEm = 0;
+    if (this.props.v_align === "center" || this.props.v_align === "center_baseline") {
+        offsetEm = -0.5 * (lines.length - 1) * lineSpacing;
+    } else if (this.props.v_align === "bottom") {
+        offsetEm = -(lines.length - 1) * lineSpacing;
+    }
+
+    this.obj.append("tspan").text(lines[0])
+        .attr("dy", offsetEm + "em");
+    for (var i = 1; i < lines.length; i++) {
+        this.obj.append("tspan").text(lines[i])
+            .attr("dy", lineSpacing + "em");
+    }
+};
+
 mpld3_Text.prototype.applyTransform = function() {
     var pos = this.coords.xy(this.position);
     this.obj.attr("x", pos[0]).attr("y", pos[1]);
+    this._applyMultilineAlignment(this.obj.selectAll("tspan"), pos[0]);
 
     if (this.props.rotation)
         this.obj.attr("transform", "rotate(" + this.props.rotation + "," + pos + ")");
-}
+};
+
+mpld3_Text.prototype._applyMultilineAlignment = function(tspans, anchorX) {
+    if (tspans.size() <= 1) {
+        tspans.attr("x", anchorX);
+        return;
+    }
+
+    function normalizeAlign(value, fallback) {
+        if (value === "start") return "left";
+        if (value === "middle") return "center";
+        if (value === "end") return "right";
+        if (value === "left" || value === "center" || value === "right") return value;
+        return fallback;
+    }
+
+    var hAlign = normalizeAlign(this.props.h_anchor, "left");
+    var mAlign = normalizeAlign(this.props.m_align, hAlign);
+    var widths = [];
+    var maxWidth = 0;
+
+    tspans.each(function() {
+        var width = this.getComputedTextLength();
+        widths.push(width);
+        if (width > maxWidth) {
+            maxWidth = width;
+        }
+    });
+
+    var bboxLeft = anchorX;
+    if (hAlign === "center") {
+        bboxLeft = anchorX - maxWidth / 2;
+    } else if (hAlign === "right") {
+        bboxLeft = anchorX - maxWidth;
+    }
+
+    tspans.each(function(_, i) {
+        var width = widths[i];
+        var x = bboxLeft;
+        if (mAlign === "center") {
+            x = bboxLeft + (maxWidth - width) / 2;
+        } else if (mAlign === "right") {
+            x = bboxLeft + (maxWidth - width);
+        }
+        d3.select(this)
+            .attr("x", x)
+            .style("text-anchor", "start");
+    });
+};
 
 // TODO: (@vladh) Remove legacy zooming code.
 // mpld3_Text.prototype.zoomed = function() {
